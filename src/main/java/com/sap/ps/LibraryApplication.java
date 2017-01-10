a4679f934967500e5b289026b866ced4c4a37756
Yu <feifei.yu@sap.com>
2017-01-10 14:00:11 +0800

    support independently deploy

--- src/main/java/com/sap/ps/LibraryApplication.java
+++ src/main/java/com/sap/ps/LibraryApplication.java
@@ -2,12 +2,13 @@ package com.sap.ps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import com.sap.icd.odatav2.spring.SpringODataLibraryPackageMarker;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = { SpringODataLibraryPackageMarker.class, LibraryApplication.class })
-public class LibraryApplication {
+public class LibraryApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
