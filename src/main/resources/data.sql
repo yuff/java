0d8afda469c8ba9b59d1a252333ec57a144d1aa4
Yu <feifei.yu@sap.com>
2017-01-12 14:08:13 +0800

    update static insert sql use sequence as id

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into BOOKS (ID, TITLE, DESCRIPTION, AUTHOR, STATE) values (1, 'Java 8 in Action', '', 'Alan Mycroft', 'unavailable' );
-insert into BOOKS (ID, TITLE, DESCRIPTION, AUTHOR, STATE) values (2, 'Spring 8 in Action', '', '', 'available' );
+insert into BOOKS (ID, TITLE, DESCRIPTION, AUTHOR, STATE) values (S_BOOK.NEXTVAL, 'Java 8 in Action', '', 'Alan Mycroft', 'unavailable' );
+insert into BOOKS (ID, TITLE, DESCRIPTION, AUTHOR, STATE) values (S_BOOK.NEXTVAL, 'Spring 8 in Action', '', '', 'available' );
 
-insert into USERS (ID, NAME, FIRST_NAME,LAST_NAME,DISPLAY_NAME,EMAIL) values(1,'I336048','Feifei', 'Yu', 'Yu Feifei','feifei.yu@sap.com');
+insert into USERS (ID, NAME, FIRST_NAME,LAST_NAME,DISPLAY_NAME,EMAIL) values(S_USER.NEXTVAL,'I336048','Feifei', 'Yu', 'Yu Feifei','feifei.yu@sap.com');
 
 insert into BORROWS (ID, BOOK_ID,USER_ID, STATE) values(S_BORROW.NEXTVAL,1,1, 'borrow');
